89ef999048a0ec195ac39472753132710cb5872e
Nicole <nicole.pan1127@gmail.com>
2021-11-05 13:56:57 -0400

    cleaning up code, deleting database, auth, etc

--- client/index.js
+++ client/index.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
 import App from './App'
 
 ReactDOM.render(
-  <Provider store={store}>
-      <App />
-  </Provider>,
+      <App />,
   document.getElementById('app')
 )
